243e4a8983898e17d4c15a5a2d3b9e35791af99d
spicyshibe <r.rasydan.w@gmail.com>
2026-04-29 12:15:52 +0700

    perang telah usai

--- external.js
+++ external.js
@@ -10,6 +10,7 @@ function ganti_gambar() {
         }
 
 
+// ini buat tombol play/stop di galeri
 const audiomedia = document.getElementById("iniaudionya")
 
 function play_audio() {
@@ -20,3 +21,87 @@ function stop_audio(){
     audiomedia.pause();
     audiomedia.currentTime = 0;
 }
+//-------------------------------------
+
+let arrayMember = JSON.parse(localStorage.getItem("datainputan")) || [];
+
+const formPendaftaran = document.getElementById("formpendaftaran");
+
+if (formPendaftaran) {
+    formPendaftaran.addEventListener("submit", function(event) {
+        
+        event.preventDefault();
+
+        const yakin = confirm("Apakah data sudah benar?");
+
+        if (!yakin){
+            return;
+        }
+
+        const namaform = document.getElementById("formnama").value;
+        const emailform = document.getElementById("formemail").value;
+        const minatform = document.getElementById("formminat").value;
+
+        const dataBaru = {
+            nama: namaform,
+            email: emailform,
+            minat: minatform
+        };
+        arrayMember.push(dataBaru);
+
+        localStorage.setItem("datainputan", JSON.stringify(arrayMember));
+
+        alert(
+            "Data Berhasil Disimpan!\n\n" +
+            "Nama: " + namaform + "\n" +
+            "Email: " + emailform + "\n" +
+            "Bidang Minat: " + minatform
+        );
+
+        this.reset();
+    });
+}
+
+function load_tabel() {
+
+    const saved = localStorage.getItem("datainputan");
+    const tabeldata = document.getElementById("tabelmember");
+    const arrayMember = JSON.parse(saved) || [];
+
+    if (tabeldata) {
+        tabeldata.innerHTML = ""; 
+
+        arrayMember.forEach(function(item, index){
+
+            let teksMinat = "";
+            switch(item.minat) {
+                case "1":
+                    teksMinat = "Cyber Security";
+                    break;
+                case "2":
+                    teksMinat = "Database";
+                    break;
+                case "3":
+                    teksMinat = "Programming";
+                    break;
+                case "4":
+                    teksMinat = "Networking";
+                    break;
+                default:
+                    teksMinat = item.minat;
+            }
+
+            const row = `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${item.nama}</td>
+                <td>${item.email}</td>
+                <td>${teksMinat}</td>
+            </tr>
+            `;
+            tabeldata.innerHTML += row;
+        });
+    }
+}
+
+load_tabel();
